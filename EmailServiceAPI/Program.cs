0212e2ad5e2ed71b288ce8bc71b6db9fa5fbd92f
Galang Rifki <galangdecimo112@gmail.com>
2025-02-10 23:49:34 +0700

    First Commit

--- EmailServiceAPI/Program.cs
+++ EmailServiceAPI/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddTransient<MainProvider>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
